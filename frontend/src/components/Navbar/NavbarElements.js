2bfbe92f46564a5b7e6b12d8bb0da3af0b70a82d
Santiago Feijoo <santiago.feijoo96@gmail.com>
2022-02-24 00:06:15 -0300

    navbar fixed

--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background: inherits;
+    background: whitesmoke;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
     width: 100%;
 `
